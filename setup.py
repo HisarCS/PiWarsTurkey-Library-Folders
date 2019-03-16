5513808f7bd98cbf40a9616efa34eebfcfdb2afe
Yaşar İdikut <yasar.idikut@hisarschool.k12.tr>
2019-03-16 12:11:52 +0300

    added prints

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup
 with open("README.md", "r") as fh:
       long_description = fh.read()
 
-print("PiWarsTurkiyeRobotKiti2019 kutuphanesine hosgeldiniz!")
-print("gerekli dokumantosyonu https://github.com/HisarCS/PiWarsTurkey-Library-Folders adresinden bulabilirsiniz.")
-print("Herhangi bir sorun cikarsa once yukaridaki dokumantosyona goz atmanizi, eger sorun hala devam ederse bize ulasmaktan cekinmeyin.")
-print("Bu kutuphaneyi indirirken de kullanirken de programin sudo ile calistigindan emin olun.")
+
+print("                                                                                 ")
+print("*********************************************************************************")
+print("         PiWarsTurkiyeRobotKiti2019 kutuphanesine hosgeldiniz!")
+print("         Gerekli dokumantosyonu https://github.com/HisarCS/PiWarsTurkey-Library-Folders adresinden bulabilirsiniz.")
+print("         Herhangi bir sorun cikarsa once yukaridaki dokumantosyona goz atmanizi, eger sorun hala devam ederse bize ulasmaktan cekinmeyin.")
+print("         Bu kutuphaneyi indirirken de kullanirken de programin sudo ile calistigindan emin olun.")
+print("*********************************************************************************")
+print("                                                                                 ")
 
 setup(
     name = "PiWarsTurkiyeRobotKiti2019",
