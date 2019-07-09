b0206007d2fcfad81fe61e40085a79ecc30c8d71
YoelK <yoelkastro@gmail.com>
2019-07-09 14:42:08 +0300

    Gave run_motor command arguments and options

--- CommandLineTools.py
+++ CommandLineTools.py
@@ -1,8 +1,23 @@
 import click
+#import PiWarsTurkiyeRobotKiti2019 as pitr
 
 @click.group()
 def main():
 	print("main")
+
 @main.command()
-def run_motor():
-	print("Running motors")
+@click.option("--speed", "-s", default=480, help="The speed of the motors")
+@click.argument("motors")
+def run_motor(speed,motors):
+	speeds = [0, 0]
+	if(motors == "left"):
+		speeds[0] = speed
+	elif(motors == "right"):
+		speeds[1] = speed
+	elif(motors == "both"):
+		speeds[0] = speed
+		speeds[1] = speed
+	else:
+		click.echo("Not a valid argument")
+		return
+	click.echo(speeds)
